8a52b870bce4e959d810bfe2b290e9a8fb505de7
kirstenflo <floyd.kirsten@yahoo.com>
2019-06-25 13:14:19 -0400

    added server

--- tic-tac-toe-test/server-test.js
+++ tic-tac-toe-test/server-test.js
@@ -1,3 +1,30 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+
+    function callback(err, data) {``
+        console.log(req.url);
+        if (err) throw err;
+        let contentType = "text/html";
+        if (req.url === '/style-test.css') contentType = "text/css";
+        if (req.url === '/server-test.js') contentType = "application/javascript";
+        console.log(contentType);
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.write(data);
+        res.end();
+    }
+    if (req.url === '/') fs.readFile('./display-test.html', callback)
+    else if (req.url !== '/favicon.ico'){
+        console.log(req.url);
+        fs.readFile("." + req.url, callback);
+    }
+
+}).listen(8000);
+
+
+
+//game
 var board = [ // 2D array signifying the board
     [0, 0, 0],
     [0, 0, 0],
